0ed1a92a0054aa30c192efb2e7fe1223182e1165
Msxali <gzirishvilidatuna@gmali.com>
2024-07-06 17:21:42 +0400

    first commit

---
+++ davaleba lvl4/davalebalvl4n2.py
@@ -0,0 +1,10 @@
+name=input("enter ur name")
+surname=input("enter ur surname")
+age=input(str("enter ur age"))
+city=input("enter ur city")
+
+print(name + surname + age + " ur city is " + city)
+
+
+
+
