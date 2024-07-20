6cddda100f06fde6dd84dd7387115bd2c51d820b
Msxali <gzirishvilidatuna@gmali.com>
2024-07-21 00:37:38 +0400

    second commit

---
+++ level9/homework/homework1.py
@@ -0,0 +1,8 @@
+#შექმენით დროში მოგზაურობის პროგრამა, რომელიც მომხმარებელს შეეკითხება მის ამჟამინდელ ასაკს, ამჟამინდელ წელს, რამდენი წლით სურს დროში მოგზაურობა, შემდეგ კი პროგრამა დაბეჭდავს დროში მოგზაურობის შემდეგ რომელი წელი იქნება და რამდენი წლის იქნება მომხმარებელი დროში მოგზაურობის შემდეგ
+
+year = int(input('input current year'))
+age=   int( input('input how old are u'))
+fiture= int(input("enter numbre that u wnt to go in fiture"))
+
+print(year + fiture)
+print (age + fiture)
