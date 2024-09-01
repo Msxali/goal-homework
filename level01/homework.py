f4e1d6df6512e2038f4a7bd03661de6f0b5b8b91
Msxali <gzirishvilidatuna@gmali.com>
2024-09-01 13:32:20 +0400

    second commit

---
+++ level01/homework.py
@@ -0,0 +1,6 @@
+name="datuna"
+surname="gzirishvili"
+age="15"
+haight="179"
+
+print("my name is"+ name + " and my surname is " + surname + " I am  " + str(age) + " year old and i am " + str(haight) + "cm in haight")
